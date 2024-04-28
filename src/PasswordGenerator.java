62a037a46862912c18e8ec421e933ccc745cdf56
RealPummel <148569022+RealPummel@users.noreply.github.com>
2024-04-29 00:21:41 +0200

    .

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class PasswordGenerator {
 
     public static void main(String[] args) {
         System.out.println("   ___                                 __  _____                      __          \n" +
